0624f05e5db58b4d3de8fb841ec181104769284c
Aabishkar Aryal <aabiskar1@live.com>
2025-04-27 11:16:24 +0100

    chore: add formatting and linting scripts to package.json
    - Added "check-format" script to verify code formatting with Prettier.
    - Added "format" script to automatically format code using Prettier.
    - Added "prepare" script to install Husky for Git hooks.
    - Updated dependencies to include lint-staged, prettier, and sort-package-json.

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+const lintstagedConfig = {
+  "*.{js,ts}": ["prettier --write"],
+  "*.{json,md,yaml,yml}": "prettier --write",
+  "package.json": ["prettier --write", "sort-package-json"],
+};
+
+export default lintstagedConfig;
